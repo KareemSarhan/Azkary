8ff05d73419250802274fa3c26ba507144e163be
Kareem Sarhan <kareemsarhan@gmail.com>
2026-03-03 16:36:08 +0200

    chore: bump version to 3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.app.azkary"
         minSdk = 33
         targetSdk = 36
-        versionCode = 11
-        versionName = "3.0.1"
+        versionCode = 12
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
