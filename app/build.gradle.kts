60660a06e3ae9fa7c0ceaccaefe9a3dbdda49a44
Kareem Sarhan <kareemsarhan@gmail.com>
2026-06-24 17:22:30 +0300

    Bump version for self-hosted republish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.app.azkary"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "3.1.3"
+        versionCode = 22
+        versionName = "3.1.4"
 
         testInstrumentationRunner = "com.app.azkary.HiltTestRunner"
         vectorDrawables {
